65d3771ee752b8ae1bec09aefcdd403b731f704d
Frnklyn <rortizjhonfrank@uss.edu.pe>
2025-05-30 16:20:53 -0500

    add

--- api/models/condicion_promocion.py
+++ api/models/condicion_promocion.py
@@ -2,6 +2,7 @@ from django.db import models
 from .promocion import Promocion
 from .producto import Producto
 from .linea_producto import LineaProducto
+from .canal_cliente import CanalCliente
 
 class CondicionPromocion(models.Model):
     promocion = models.ForeignKey(Promocion, on_delete=models.CASCADE, related_name='condiciones')
@@ -10,6 +11,7 @@ class CondicionPromocion(models.Model):
     valor_max = models.FloatField(null=True, blank=True)
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True, related_name='condiciones')
     linea_producto = models.ForeignKey(LineaProducto, on_delete=models.SET_NULL, null=True, blank=True, related_name='condiciones')
+    canal_cliente = models.ForeignKey(CanalCliente, on_delete=models.SET_NULL, null=True, blank=True, related_name='condiciones')
     bonificacion_descuento = models.BooleanField()
 
     class Meta:
